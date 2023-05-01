2fa7aaf8d54abc5ae4167331569f8c32f38a5683
sait han uzun <saithan.uzun@gmail.com>
2023-05-01 22:08:46 +0100

    Add Strings

---
+++ Add-Strings/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Given two non-negative integers,
+ * num1 and num2 represented as string, return the sum of num1 and num2 as a string.
+ */
+
+using System.Text;
+
+class Program
+{
+    public static void Main()
+    {
+        string input1 = "234234", input2 = "234234";
+
+        Console.WriteLine(Solution.AddStrings(input1, input2));
+    }
+}
+
+public static class Solution
+{
+    public static string AddStrings(string num1, string num2)
+    {
+        StringBuilder sb = new();
+
+        int maxLength = num1.Length > num2.Length ? num1.Length : num2.Length;
+
+        var number1 = num1.Reverse().ToArray().SetArray(maxLength);
+        var number2 = num2.Reverse().ToArray().SetArray(maxLength);
+
+        int carry = 0;
+        int i = 0;
+        while ( i < maxLength || carry > 0)
+        {
+            if (i >= maxLength)
+            {
+                sb.Insert(0, carry);
+                break;
+            }
+            
+            int sum = carry + number1[i].GetInt() + number2[i].GetInt();
+            carry = 0;
+
+            if (sum > 9)
+            {
+                carry = sum / 10;
+                sb.Insert(0, sum % 10);
+                i++;
+                continue;
+            }
+
+            sb.Insert(0, sum);
+            i++;
+        }
+
+
+        return sb.ToString();
+    }
+
+    public static int GetInt(this char chr) => chr - '0';
+
+    public static char[] SetArray(this char[] array, int length)
+    {
+        if (array.Length >= length)
+        {
+            return array;
+        }
+
+        char[] result = new char[length];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            result[i] = array[i];
+        }
+
+        for (int i = array.Length; i < length; i++)
+        {
+            result[i] = '0';
+        }
+
+        return result;
+    }
+}
--- Add-Two-Numbers/Program.cs
+++ Add-Two-Numbers/Program.cs
@@ -3,6 +3,7 @@
  * The digits are stored in reverse order, and each of their nodes contains a single digit.
  * Add the two numbers and return the sum as a linked list.
  * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * 
  */
 
 using System.Text;
