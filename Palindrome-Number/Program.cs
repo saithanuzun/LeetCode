1ccf25756e4ffc10b93830e98ae7cb2b969f94df
sait han uzun <saithan.uzun@gmail.com>
2023-05-04 19:21:53 +0100

    Palindrome-Number

---
+++ Palindrome-Number/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(new Solution().IsPalindrome(101));
+    }
+}
+
+class Solution
+{
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+
+        int number = x;
+        int number2 = 0;
+        int i = 0;
+
+        while (x != 0)
+        {
+            number2 = number2 * 10 + x % 10;
+            i++;
+            x /= 10;
+        }
+
+        return number == number2;
+    }
+}
