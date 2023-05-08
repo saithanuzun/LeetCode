f231492c820738386e0718ebdd863631aa19fb85
sait han uzun <saithan.uzun@gmail.com>
2023-05-08 23:27:26 +0100

    Valid Parentheses

---
+++ Valid-Parentheses/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * 
+ */
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(new Solution().IsValid("(()[]())"));
+    }
+}
+
+class Solution
+{
+    public bool IsValid(string s)
+    {
+        Stack<char> st = new();
+        var charArray = s.ToCharArray();
+
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            if (charArray[i] == '(' || charArray[i] == '[' || charArray[i] == '{')
+            {
+                st.Push(charArray[i]);
+                continue;
+            }
+
+            if (st.Count == 0)
+                return false;
+
+            char top = st.Pop();
+            
+            if (top == '(' && charArray[i] != ')')
+                return false;
+
+            if (top == '[' && charArray[i] != ']')
+                return false;
+
+            if (top == '{' && charArray[i] != '}')
+                return false;
+        }
+
+        return st.Count == 0;
+    }
+}
